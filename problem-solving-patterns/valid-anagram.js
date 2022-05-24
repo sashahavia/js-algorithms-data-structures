3abe24f0895ce0bc24aa42ce1228586c85dde44f
Sasha Havia <sasha.havia@gmail.com>
2022-05-24 19:26:01 -0400

    added validAnagram function

---
+++ problem-solving-patterns/valid-anagram.js
@@ -0,0 +1,42 @@
+/**
+ * Given two strings, wrtite a function to determine if the seod string is an anagram of the first.
+ * An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman
+ * Note: You may asume the string contains only lowercase alphabets
+ *
+ * Time Complexity: O(n)
+ *
+ * @param {*} str1
+ * @param {*} str2
+ */
+
+function validAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+
+  for (let char of str1) {
+    frequencyCounter1[char] = ++frequencyCounter1[char] || 1;
+  }
+  for (let char of str2) {
+    frequencyCounter2[char] = ++frequencyCounter2[char] || 1;
+  }
+  for (let key in frequencyCounter1) {
+    if (!frequencyCounter2[key]) {
+      return false;
+    }
+    if (frequencyCounter1[key] !== frequencyCounter2[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// validAnagram("", ""); // true
+// validAnagram('aaz', 'zza'); // false
+// validAnagram('anagram', 'nagaram'); // true
+// validAnagram('rat', 'car'); // false
+// validAnagram('awesome', 'awesom'); // false
+// validAnagram('qwerty', 'qeywrt'); // true
+// validAnagram('texttwisttime', 'timetwisttext'); // true
