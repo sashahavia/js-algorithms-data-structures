76c3728bdcb30f5f861137b44427005622698537
Sasha Havia <sasha.havia@gmail.com>
2022-06-02 16:53:27 -0400

    Created isSubsequence function

---
+++ src/problem-solving-patterns/is-subsequence.js
@@ -0,0 +1,21 @@
+/**
+* Write a function called isSubsequence which takes in two strings and checks  whether
+* the characters in the first string form a subsequence of the characters in the second  string. 
+* In other words, the function should check whether the characters in the first string  appear somewhere  
+* in the second string, without their order changing. 
+*/
+
+export function isSubsequence(str1, str2) { 
+  let i = 0;
+  let j = 0;
+  if (!str1) return true;
+
+  while (j < str2.length) { 
+    if (str2[j] === str1[i]) i++;
+     
+    if (i === str1.length) return true; 
+    j++; 
+  } 
+  return false; 
+} 
+ 
---
+++ src/problem-solving-patterns/is-subsequence.test.js
@@ -0,0 +1,19 @@
+import {isSubsequence} from './is-subsequence';
+
+describe('isSubsequence', () => {
+  it('should return true for isSubsequence("hello", "hello world")', () => {
+    expect(isSubsequence('hello', 'hello world')).toBeTruthy();
+  });
+
+  it('should return true for isSubsequence("sing", "sting")', () => {
+    expect(isSubsequence('sing', 'sting')).toBeTruthy();
+  });
+
+  it('should return true for isSubsequence("abc", "abracadabra")', () => {
+    expect(isSubsequence('ab', 'abracadabra')).toBeTruthy();
+  });
+
+  it('should return false for isSubsequence("abc", "acb")', () => {
+    expect(isSubsequence('abc', 'acb')).toBeFalsy();
+  });
+});
