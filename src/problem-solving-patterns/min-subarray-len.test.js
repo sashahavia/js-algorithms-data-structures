d1fb887cb84a6d56ad7520b8f9f8bc4d6e796a57
Sasha Havia <sasha.havia@gmail.com>
2022-06-06 09:34:47 -0400

    Added tests for minSubArrayLen

--- src/problem-solving-patterns/min-subarray-len.test.js
+++ src/problem-solving-patterns/min-subarray-len.test.js
@@ -9,7 +9,7 @@ describe('minSubArrayLen', () => {
     expect(minSubArrayLen([2, 1, 6, 5, 4], 9)).toEqual(2);
   });
 
-  it('should return 1 for the array [3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19] and number 7', () => {
+  it('should return 1 for the array [3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19] and number 52', () => {
     expect(minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52)).toEqual(1);
   });
 
