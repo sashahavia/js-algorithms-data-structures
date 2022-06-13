6d3c29ffc961407c6b60475438d71cd16099d106
Sasha Havia <sasha.havia@gmail.com>
2022-06-13 09:31:08 -0400

    Created nestedEvenSum function

--- src/challenging-recursion/nested-even-sum.js
+++ src/challenging-recursion/nested-even-sum.js
@@ -6,7 +6,14 @@
  */
 
 export const nestedEvenSum = (obj, sum = 0) => {
+  for (let key in obj) {
+    if (typeof obj[key] === 'object') {
+      sum += nestedEvenSum(obj[key]);
+    }
 
-  
+    if (typeof obj[key] === 'number' && obj[key] % 2 === 0) {
+      sum += obj[key];
+    }
+  }
   return sum;
 };
--- src/challenging-recursion/nested-even-sum.test.js
+++ src/challenging-recursion/nested-even-sum.test.js
@@ -3,11 +3,29 @@ import { nestedEvenSum } from './nested-even-sum';
 const obj1 = {
   outer: 2,
   obj: {
-
+    inner: 2,
+    otherObj: {
+      superInner: 2,
+      notANumber: true,
+      alsoNotANumber: 'yup'
+    }
   }
 };
 
-describe('nestedEvenSum', () => {
-  
+const obj2 = {
+  a: 2,
+  b: {b: 2, bb: {b: 3, bb: {b: 2}}},
+  c: {c: {c: 2}, cc: 'ball', ccc: 5},
+  d: 1,
+  e: {e: {e: 2}, ee: 'car'}
+};
 
+describe('nestedEvenSum', () => {
+  it('should return 6 for obj1', () => {
+    expect(nestedEvenSum(obj1)).toEqual(6);
+  });
+
+  it('should return 10 for obj2', () => {
+    expect(nestedEvenSum(obj2)).toEqual(10);
+  });
 });
