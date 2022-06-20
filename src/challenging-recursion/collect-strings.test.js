dd1fc6ab1ce18d91268746797bd00bb02075ef72
Sasha Havia <sasha.havia@gmail.com>
2022-06-20 09:42:16 -0400

    added tests for collectStrings

---
+++ src/challenging-recursion/collect-strings.test.js
@@ -0,0 +1,29 @@
+import {collectStrings} from './collect-strings';
+
+describe('collectStrings', () => {
+  const obj = {
+    stuff: 'foo',
+    data: {
+      val: {
+        thing: {
+          info: 'bar',
+          moreInfo: {
+            evenMoreInfo: {
+              weMadeIt: 'baz'
+            }
+          }
+        }
+      }
+    }
+  };
+
+  it('should return an array ["foo", "bar", "baz"] of all strings in obj', () => {
+    const expectedResult = ['foo', 'bar', 'baz'];
+
+    expect(collectStrings(obj)).toEqual(expectedResult);
+  });
+
+  it('should return empty array for empty obj', () => {
+    expect(collectStrings({})).toEqual([]);
+  });
+});
